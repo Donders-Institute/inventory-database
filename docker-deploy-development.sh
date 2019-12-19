f8ff08443bf20470e4d439fd3e9025d2f89c7428
Rutger van Deelen <r.vandeelen@donders.ru.nl>
2019-12-19 15:09:34 +0100

    Fix docker image name

--- docker-deploy-development.sh
+++ docker-deploy-development.sh
@@ -2,4 +2,4 @@
 
 # export variables defined in env.sh
 set -a && source env.sh && set +a
-docker stack up -c docker-compose.yml -c docker-compose.swarm.yml --prune --with-registry-auth --resolve-image always inventory-database
+docker stack up -c docker-compose.yml -c docker-compose.swarm.yml --prune --with-registry-auth --resolve-image always inventory-database-ui
--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,4 +2,4 @@
 
 # export variables defined in env.sh
 set -a && source env.sh && set +a
-docker stack up -c docker-compose.yml -c docker-compose.swarm.yml --with-registry-auth inventory-database
+docker stack up -c docker-compose.yml -c docker-compose.swarm.yml --with-registry-auth inventory-database-ui
